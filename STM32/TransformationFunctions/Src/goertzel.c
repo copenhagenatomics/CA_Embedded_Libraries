afce7aecfaf965a09a72d864ef7a29590f949af5
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2024-04-24 08:24:43 +0200

    Update goertzel and add tests

--- STM32/TransformationFunctions/Src/goertzel.c
+++ STM32/TransformationFunctions/Src/goertzel.c
@@ -2,9 +2,9 @@
 #include "math.h"
 
 static Goertzel hGoertzel;
+static float q0, q1, q2 = 0;
+static int sample_no = 0;
 
-// Initialize the Goertzel filter. 
-// Explanations of the parameters are found in goertzel.h
 /* Input Parameters:
 
 	targetFrequency: 	The frequency [Hz] of which the magnitude should be computed
@@ -13,52 +13,60 @@ static Goertzel hGoertzel;
 	adcres:				The maximum ADC value 12-bit: 4096, 16-bit: 65536
 	vRange:				Voltage range of the ADC typically 3.3V
 	gain:				Any PCB specific gain from schematic. 
+	vToUnit:			Conversion factor from Voltage to other unit (bias independent)
 */ 
-void GoertzelInit(int targetFrequency, int sampleRate, float samplesPerOutput, float adcres, float vRange, float gain)
+void GoertzelInit(float adcres, float vRange, float gain, int targetFrequency, int sampleRate, float samplesPerOutput, float vToUnit)
 {
-	hGoertzel.samplesPerOutput = samplesPerOutput;	
-	hGoertzel.k = (int) (0.5 + ((hGoertzel.samplesPerOutput * targetFrequency) / sampleRate));
-	hGoertzel.omega = (2.0 * M_PI * hGoertzel.k) / hGoertzel.samplesPerOutput;
-	hGoertzel.sine = sin(hGoertzel.omega);
-	hGoertzel.cosine = cos(hGoertzel.omega);
-	hGoertzel.coeff = 2.0 * hGoertzel.cosine;
-	hGoertzel.scalingFactor = 1.0/(hGoertzel.samplesPerOutput / 2.0); // Reciprocal scalingFactor for speed.
-	hGoertzel.inputScaling = 1.0/(adcres*vRange/gain); // Reciprocal of inputScaling for speed.
+    // As the frequency resolution of a "DFT" operation is df = (fs/NDFT) and our target
+    // frequency is 2000kHz then we need minimally df = sampleRate/x = 2000Hz -> x = sampleRate/targetFrequency = 100 samples
+    // Given the updating frequency on the controller side comes at 1000Hz we choose 200 samples to fit the dynamics.  
+    hGoertzel.samplesPerOutput = samplesPerOutput;	
+    hGoertzel.k = (int) (0.5 + ((hGoertzel.samplesPerOutput * targetFrequency) / sampleRate));
+    hGoertzel.omega = (2.0 * M_PI * hGoertzel.k) / hGoertzel.samplesPerOutput;
+    hGoertzel.sine = sin(hGoertzel.omega);
+    hGoertzel.cosine = cos(hGoertzel.omega);
+    hGoertzel.coeff = 2.0 * hGoertzel.cosine;
+    hGoertzel.scalingFactor = 1.0/(hGoertzel.samplesPerOutput / 2.0); // Reciprocal scalingFactor for speed.
+    hGoertzel.inputScaling = (1.0/(adcres/vRange*gain))*vToUnit; // Reciprocal of inputScaling for speed.
 }
 
-// All computation that can be performed once are intialized in GoertzelInit().
+void resetGoertzelParameters()
+{
+    sample_no = 0;
+    q0 = q1 = q2 = 0;
+}
+
+// Computation of signal power specifically 2kHz using the Goertzel algorithm.
+// All computation that can be performed once are intialized in GoertzelInit(...).
 // Theory: https://en.wikipedia.org/wiki/Goertzel_algorithm
 // Inspired by: https://stackoverflow.com/a/11581251/7683649
 // Returns 1 when data magnitude has been updated, otherwise 0.
-int computeSignalPower(int32_t *pData, int noOfChannels, int noOfSamples, float * magnitude)
+int computeSignalPower(int32_t *pData, int noOfChannels, int noOfSamples, int channel, float * magnitude)
 {
-    static float q0, q1, q2 = 0;
-	static int count = 0;
+    // The full length of the Goertzel filter is 200. The interrupt only hold 5 samples
+    // i.e the filtering process is run 200/5 = 40 times before returning the magnitude. 
+    for (int i = 0; i < noOfSamples; i++)
+    {
+        q0 = hGoertzel.coeff * q1 - q2 + pData[i*noOfChannels + channel]*hGoertzel.inputScaling;
+        q2 = q1;
+        q1 = q0;
+    }
 
-	for (int i = 0; i < noOfSamples; i++)
-	{
-		q0 = hGoertzel.coeff * q1 - q2 + (pData[i*noOfChannels]*hGoertzel.inputScaling);
-		q2 = q1;
-		q1 = q0;
-	}
+    // We need to reset the filter outputs q0, q1, q2 after every samplesPerOutput samples (hGoertzel.samplesPerOutput/noOfSamples iterations)
+    // As the IIR filter will otherwise become unstable with high probability.
+    // Explanation: https://dsp.stackexchange.com/a/30308
+    sample_no++;
+    if (sample_no >= hGoertzel.samplesPerOutput/noOfSamples)
+    {
+        // Compute the real part, imaginary part and magnitude
+        float real = (q1 - q2 * hGoertzel.cosine) * hGoertzel.scalingFactor;
+        float imag = (q2 * hGoertzel.sine) * hGoertzel.scalingFactor;
 
-	// The full length of the Goertzel filter is hGoertzel.samplesPerOutput. The interrupt hold noOfSamples samples
-	// where noOfSamples <= hGoertzel.samplesPerOutput i.e the filtering process is 
-	// run hGoertzel.samplesPerOutput/noOfSamples iterations before returning the magnitude. 
-	// We need to reset the filter outputs q0, q1, q2 after every hGoertzel.samplesPerOutput samples 
-	// As the IIR filter will otherwise become unstable with high probability.
-	// Explanation: https://dsp.stackexchange.com/a/30308
-	count++;
-	if (count >= hGoertzel.samplesPerOutput/noOfSamples)
-	{
-		// Compute the real part, imaginary part and magnitude
-		float real = (q1 - q2 * hGoertzel.cosine) * hGoertzel.scalingFactor;
-		float imag = (q2 * hGoertzel.sine) * hGoertzel.scalingFactor;
-		*magnitude = sqrtf(real*real + imag*imag);
+        *magnitude =  sqrtf(real*real + imag*imag);
 
-		count = 0;
-		q0 = q1 = q2 = 0;
-		return 1;
-	}
-	return 0;
+        sample_no = 0;
+        q0 = q1 = q2 = 0;
+        return 1;
+    }
+    return 0;
 }
---
+++ unit_testing/TransformationFunctions/goertzel_tests.cpp
@@ -0,0 +1,267 @@
+/*!
+** @file   goertzel_tests.cpp
+** @author Matias
+** @date   23/04/2024
+*/
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <cmath>
+
+/* Fakes */
+
+/* Real supporting units */
+
+/* UUT */
+#include "goertzel.h"
+
+using namespace std;
+
+/***************************************************************************************************
+** TEST FIXTURES
+***************************************************************************************************/
+
+class GoertzelTest: public ::testing::Test 
+{
+    protected:
+        /*******************************************************************************************
+        ** METHODS
+        *******************************************************************************************/
+        GoertzelTest() {
+
+
+
+        }
+
+        static void generateSine(int32_t* pData, int noOfSamples, float amplitude, float offset, float freq, float fs)
+        {
+            const float Ts = 1.0/fs;
+            for (int i = 0; i<noOfSamples; i++)
+                pData[i] = offset + (amplitude * sin( 2*M_PI*(i*Ts)*freq ));
+        }
+
+    // Variables needed for the sine generation and the goertzel filter
+    float fs = 50000.0;
+    int noOfSamples = 50;
+    int targetFreq = 2000;
+    float peakToPeak = 16777216.0;
+    float amplitude = peakToPeak/2.0;
+    float offset = 0;
+    float vToUnit = 1;
+    float vRange = 3.3;
+    float gain = 1;
+};
+
+/***************************************************************************************************
+** TESTS
+***************************************************************************************************/
+
+TEST_F(GoertzelTest, testNoOffsetNoOffset)
+{
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+    EXPECT_NEAR(mag, vRange/2.0, tol);
+    EXPECT_EQ(ret, 1);
+}
+
+
+TEST_F(GoertzelTest, testPositiveOffset)
+{
+    // The offset should not affect the output
+    offset = amplitude;
+
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+    EXPECT_NEAR(mag, vRange/2.0, tol);
+    EXPECT_EQ(ret, 1);
+}
+
+
+TEST_F(GoertzelTest, testMCUGain)
+{
+    // Update MCU gain which should simply scale the output by the 
+    // corresponding factor
+    gain = 10.0;
+
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+    EXPECT_NEAR(mag, (vRange/2.0)/gain, tol);
+    EXPECT_EQ(ret, 1);
+}
+
+TEST_F(GoertzelTest, testVToUnitGain)
+{
+    // Update the vToUnit scalar to go from Voltage to any other unit
+    // Since the Goertzel is bias independent it does not need a vToUnitBias
+    vToUnit = 10;
+
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+    EXPECT_NEAR(mag, (vRange/2.0)*vToUnit, tol);
+    EXPECT_EQ(ret, 1);
+}
+
+TEST_F(GoertzelTest, testAllGains)
+{
+    // Having both the MCU gain and vToUnit scalar should scale the output
+    // correspondingly 
+    vToUnit = 10;
+    gain = 32.1;
+
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+    EXPECT_NEAR(mag, ((vRange/2.0)/gain)*vToUnit, tol);
+    EXPECT_EQ(ret, 1);
+}
+
+TEST_F(GoertzelTest, testMultipleCalls)
+{
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    float tol = 1e-6;
+
+    // One sample is fed to the filter at the time. 
+    // It should only compute the magnitude ones the filter has run
+    // for noOfSamples iterations.
+    for (int i = 0; i < noOfSamples; i++)
+    {
+        int ret = computeSignalPower(&pData[i], 1, 1, 0, &mag);
+        if (i==(noOfSamples-1))
+        {
+            // Return value is 1 when the magnitude has been updated.
+            EXPECT_NEAR(mag, vRange/2.0, tol);
+            EXPECT_EQ(ret, 1);
+            continue;
+        }
+        // Return value is 0 when the magnitude has not been updated.
+        EXPECT_EQ(mag, 0);
+        EXPECT_EQ(ret, 0);
+    }
+
+    // Reset the magnitude and run a single iteration of filtering
+    // The magnitude should not be updated and the return value should be 0.
+    mag=0;
+    int ret = computeSignalPower(&pData[0], 1, 1, 0, &mag);
+    EXPECT_EQ(mag, 0);
+    EXPECT_EQ(ret, 0);
+}
+
+TEST_F(GoertzelTest, testDifferingTargetAndSineFrequency)
+{
+    float sineFreq = 1000;
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, sineFreq, fs);
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+
+    // As there is no component of the target frequency in the input
+    // signal the magnitude should be very close to 0.
+    EXPECT_NEAR(mag, 0, tol);
+    EXPECT_EQ(ret, 1);
+}
+
+TEST_F(GoertzelTest, testDoubleHarmonySine)
+{
+    // Add two sines with different frequencies. One with the target
+    // frequency of the Goertzel filter and one which is some distance away
+    // from the target frequency with same amplitude.
+    // The filter should not be affected by the second frequency. 
+    float secondHarmonyFreq = 40000;
+
+    // Initialise the Goertzel filter
+    GoertzelInit(peakToPeak, vRange, gain, targetFreq, fs, noOfSamples, vToUnit);
+
+    // Create a sine that is offset to the middle point of the ADC with an
+    // amplitude that fills the whole ADC range.
+    int32_t pData[noOfSamples] = {0};
+    generateSine(pData, noOfSamples, amplitude, offset, targetFreq, fs);
+
+    int32_t pData2nd[noOfSamples] = {0};
+    generateSine(pData2nd, noOfSamples, amplitude/2, offset, secondHarmonyFreq, fs);
+
+    // Combine sines
+    for (int i = 0; i < noOfSamples; i++)
+    {
+        pData[i] += pData2nd[i];
+    }
+
+    // Filter the sine through the Goertzel filter
+    float mag=0;
+    int ret = computeSignalPower(pData, 1, noOfSamples, 0, &mag);
+
+    float tol = 1e-6;
+    EXPECT_NEAR(mag, vRange/2.0, tol);
+    EXPECT_EQ(ret, 1);
+}
