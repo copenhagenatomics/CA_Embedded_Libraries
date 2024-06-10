126fb87ade7206a990e66895754c5cfda58b11de
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2024-06-10 14:01:27 +0200

    bug fix

--- STM32/I2C/Inc/sht45.h
+++ STM32/I2C/Inc/sht45.h
@@ -18,17 +18,17 @@
 #include <stdint.h>
 
 // Valid commands can be found in section 4.5 of the datasheet
-#define SHT4X_MEASURE_HIGHREP = 0xFD,
-#define SHT4X_MEASURE_MEDREP = 0xF6,
-#define SHT4X_MEASURE_LOWREP = 0xE0,
-#define SHT4X_READ_SERIAL = 0x89,
-#define SHT4X_SOFT_RESET = 0x94,
-#define SHT4X_HEATER_200mW_1S = 0x39,
-#define SHT4X_HEATER_200mW_100mS = 0x32,
-#define SHT4X_HEATER_110mW_1S = 0x2F,
-#define SHT4X_HEATER_110mW_100mS = 0x24,
-#define SHT4X_HEATER_20mW_1S = 0x1E,
-#define SHT4X_HEATER_20mW_100mS = 0x15
+#define SHT4X_MEASURE_HIGHREP       0xFD
+#define SHT4X_MEASURE_MEDREP        0xF6
+#define SHT4X_MEASURE_LOWREP        0xE0
+#define SHT4X_READ_SERIAL           0x89
+#define SHT4X_SOFT_RESET            0x94
+#define SHT4X_HEATER_200mW_1S       0x39
+#define SHT4X_HEATER_200mW_100mS    0x32
+#define SHT4X_HEATER_110mW_1S       0x2F
+#define SHT4X_HEATER_110mW_100mS    0x24
+#define SHT4X_HEATER_20mW_1S        0x1E
+#define SHT4X_HEATER_20mW_100mS     0x15
 
 
 typedef struct {
