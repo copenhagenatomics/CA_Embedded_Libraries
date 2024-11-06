86a912a5fb4beb00c4b5a252282a957550f288a7
Luke Walker <luke.walker@copenhagenatomics.com>
2024-11-06 07:51:10 +0100

    Additions for successful unit testing

--- STM32/Util/Src/faultHandlers.c
+++ STM32/Util/Src/faultHandlers.c
@@ -7,6 +7,7 @@
  ******************************************************************************
 */
 
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -46,18 +47,18 @@ extern uint32_t _FlashAddr;
 ** Depending on fault type, further information is collected and also stored in memory.
 */
 void recordFaultType(contextStateFrame_t* frame, faultType_t faultType) {
-    faultInfo_t faultInfo = {0};
-    
     if(faultType != NO_FAULT) {
-        faultInfo.fault  = faultType;
-        faultInfo.CFSR   = SCB->CFSR;
-        faultInfo.HFSR   = SCB->HFSR;
-        faultInfo.MMFA   = SCB->MMFAR;
-        faultInfo.BFA    = SCB->BFAR;
-        faultInfo.ABFS   = SCB->AFSR;
-        faultInfo.sFrame = *frame;
+        faultInfo_t faultInfo = {
+            .fault  = faultType,
+            .CFSR   = SCB->CFSR,
+            .HFSR   = SCB->HFSR,
+            .MMFA   = SCB->MMFAR,
+            .BFA    = SCB->BFAR,
+            .ABFS   = SCB->AFSR,
+            .sFrame = *frame,
+        };
 
-        writeToFlash(0, sizeof(faultInfo_t), (void*)&faultInfo);
+        writeToFlash(0, sizeof(faultInfo_t), (uint8_t*)&faultInfo);
     }
 }
 
@@ -65,15 +66,15 @@ void recordFaultType(contextStateFrame_t* frame, faultType_t faultType) {
 ** @brief This function clears any information on faults from flash memory
 */
 void clearFaultInfo() {
-    faultInfo_t faultInfo = {.fault = -1};
-    writeToFlash(0, sizeof(faultInfo_t), (void*)&faultInfo);
+    faultInfo_t faultInfo = {.fault = NO_FAULT};
+    writeToFlash(0, sizeof(faultInfo_t), (uint8_t*)&faultInfo);
 }
 
 /*!
 ** @brief This function returns most recent fault Info as a struct
 */
 void getFaultInfo(faultInfo_t* faultInfo) {
-    readFromFlash(0, sizeof(faultInfo_t), (void*)faultInfo);
+    readFromFlash(0, sizeof(faultInfo_t), (uint8_t*)faultInfo);
 }
 
 /*!
@@ -82,21 +83,21 @@ void getFaultInfo(faultInfo_t* faultInfo) {
 bool printFaultInfo() {
     static char buf[BUF_LEN] = {0};
     int len = 0;
-    faultInfo_t fi = {0};
+    faultInfo_t fi;
     getFaultInfo(&fi);
 
     if(fi.fault != NO_FAULT) {
         len += snprintf(&buf[len], BUF_LEN - len, "\nStart of fault info\r\n");
         len += snprintf(&buf[len], BUF_LEN - len, "Last fault was: %d\r\n", fi.fault);
-        len += snprintf(&buf[len], BUF_LEN - len, "CFSR was: 0x%08lX\r\n",  fi.CFSR);
-        len += snprintf(&buf[len], BUF_LEN - len, "HFSR was: 0x%08lX\r\n",  fi.HFSR);
-        len += snprintf(&buf[len], BUF_LEN - len, "MMFA was: 0x%08lX\r\n",  fi.MMFA);
-        len += snprintf(&buf[len], BUF_LEN - len, "BFA was:  0x%08lX\r\n",  fi.BFA);
-        len += snprintf(&buf[len], BUF_LEN - len, "AFSR was: 0x%08lX\r\n",  fi.ABFS);
+        len += snprintf(&buf[len], BUF_LEN - len, "CFSR was: 0x%08" PRIx32 "\r\n",  fi.CFSR);
+        len += snprintf(&buf[len], BUF_LEN - len, "HFSR was: 0x%08" PRIx32 "\r\n",  fi.HFSR);
+        len += snprintf(&buf[len], BUF_LEN - len, "MMFA was: 0x%08" PRIx32 "\r\n",  fi.MMFA);
+        len += snprintf(&buf[len], BUF_LEN - len, "BFA was:  0x%08" PRIx32 "\r\n",  fi.BFA);
+        len += snprintf(&buf[len], BUF_LEN - len, "AFSR was: 0x%08" PRIx32 "\r\n",  fi.ABFS);
         len += snprintf(&buf[len], BUF_LEN - len, "Stack Frame was:\r\n");
-        len += snprintf(&buf[len], BUF_LEN - len, "0x%08lX, 0x%08lX, 0x%08lX, 0x%08lX\r\n", 
+        len += snprintf(&buf[len], BUF_LEN - len, "0x%08" PRIx32 ", 0x%08" PRIx32 ", 0x%08" PRIx32 ", 0x%08" PRIx32 "\r\n", 
             fi.sFrame.r0, fi.sFrame.r1, fi.sFrame.r2, fi.sFrame.r3);
-        len += snprintf(&buf[len], BUF_LEN - len, "0x%08lX, 0x%08lX, 0x%08lX, 0x%08lX\r\n",
+        len += snprintf(&buf[len], BUF_LEN - len, "0x%08" PRIx32 ", 0x%08" PRIx32 ", 0x%08" PRIx32 ", 0x%08" PRIx32 "\r\n",
             fi.sFrame.r12, fi.sFrame.lr, fi.sFrame.return_address, fi.sFrame.xpsr);
         len += snprintf(&buf[len], BUF_LEN - len, "End of fault info\r\n");
 
--- unit_testing/fakes/fake_stm32f401xc.h
+++ unit_testing/fakes/fake_stm32f401xc.h
@@ -142,6 +142,40 @@ typedef enum
 #define __IO            volatile
 #define __STATIC_INLINE static inline
 #define __RBIT          __rbit
+#define     __IM     volatile       /*! Defines 'read only' structure member permissions */
+#define     __OM     volatile            /*! Defines 'write only' structure member permissions */
+#define     __IOM    volatile            /*! Defines 'read / write' structure member permissions */
+
+typedef struct
+{
+  __IM  uint32_t CPUID;                  /*!< Offset: 0x000 (R/ )  CPUID Base Register */
+  __IOM uint32_t ICSR;                   /*!< Offset: 0x004 (R/W)  Interrupt Control and State Register */
+  __IOM uint32_t VTOR;                   /*!< Offset: 0x008 (R/W)  Vector Table Offset Register */
+  __IOM uint32_t AIRCR;                  /*!< Offset: 0x00C (R/W)  Application Interrupt and Reset Control Register */
+  __IOM uint32_t SCR;                    /*!< Offset: 0x010 (R/W)  System Control Register */
+  __IOM uint32_t CCR;                    /*!< Offset: 0x014 (R/W)  Configuration Control Register */
+  __IOM uint8_t  SHP[12U];               /*!< Offset: 0x018 (R/W)  System Handlers Priority Registers (4-7, 8-11, 12-15) */
+  __IOM uint32_t SHCSR;                  /*!< Offset: 0x024 (R/W)  System Handler Control and State Register */
+  __IOM uint32_t CFSR;                   /*!< Offset: 0x028 (R/W)  Configurable Fault Status Register */
+  __IOM uint32_t HFSR;                   /*!< Offset: 0x02C (R/W)  HardFault Status Register */
+  __IOM uint32_t DFSR;                   /*!< Offset: 0x030 (R/W)  Debug Fault Status Register */
+  __IOM uint32_t MMFAR;                  /*!< Offset: 0x034 (R/W)  MemManage Fault Address Register */
+  __IOM uint32_t BFAR;                   /*!< Offset: 0x038 (R/W)  BusFault Address Register */
+  __IOM uint32_t AFSR;                   /*!< Offset: 0x03C (R/W)  Auxiliary Fault Status Register */
+  __IM  uint32_t PFR[2U];                /*!< Offset: 0x040 (R/ )  Processor Feature Register */
+  __IM  uint32_t DFR;                    /*!< Offset: 0x048 (R/ )  Debug Feature Register */
+  __IM  uint32_t ADR;                    /*!< Offset: 0x04C (R/ )  Auxiliary Feature Register */
+  __IM  uint32_t MMFR[4U];               /*!< Offset: 0x050 (R/ )  Memory Model Feature Register */
+  __IM  uint32_t ISAR[5U];               /*!< Offset: 0x060 (R/ )  Instruction Set Attributes Register */
+        uint32_t RESERVED0[5U];
+  __IOM uint32_t CPACR;                  /*!< Offset: 0x088 (R/W)  Coprocessor Access Control Register */
+} SCB_Type;
+
+extern SCB_Type SCB_obj;
+
+#define SCB ((SCB_Type *) &SCB_obj)
+
+// End of #include "core_cm4.h"
 
 extern uint32_t SystemCoreClock;          /*!< System Clock Frequency (Core Clock) */
 extern const uint8_t  AHBPrescTable[16];    /*!< AHB prescalers table values */
