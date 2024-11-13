ac0889cb3a5ffe1269ba7ac7099f5e77281a1f8c
TimotheDodinCA <timothe.dodin@copenhagenatomics.com>
2024-11-13 10:17:28 +0100

    Unit tests correction 4

--- unit_testing/ADCMonitor/adcmonitor_tests.cpp
+++ unit_testing/ADCMonitor/adcmonitor_tests.cpp
@@ -172,7 +172,7 @@ TEST_F(ADCMonitorTest, testADCmin)
 
     EXPECT_EQ(ADCmin(pData,0), 12);
     EXPECT_EQ(ADCmin(pData,1), (1-noOfSamples)*2);
-    //EXPECT_EQ(ADCmin(pData,2), 100);
+    EXPECT_EQ(ADCmin(pData,2), 100);
     //EXPECT_EQ(ADCmin(pData,3), -1947);
     //EXPECT_EQ(ADCmin(pData,4), -3994);
 }
