079fff04f990d0540e55c3e5e15b211e863f79f8
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2024-11-21 12:18:38 +0100

    fake DS18B20

---
+++ unit_testing/fakes/fake_DS18B20.cpp
@@ -0,0 +1,19 @@
+/*!
+** @brief Fake Interface to DS18B20 for unit testing
+**
+** @author Matias
+** @date   21/11/2024
+*/
+
+#include "fake_DS18B20.h"
+
+
+float getTemp()
+{
+
+}
+
+void DS18B20Init(TIM_HandleTypeDef* htim, StmGpio* gpio, GPIO_TypeDef *blk, uint16_t pin)
+{
+
+}
---
+++ unit_testing/fakes/fake_DS18B20.h
@@ -0,0 +1,16 @@
+/*!
+** @brief Fake Interface to DS18B20 for unit testing
+**
+** Extends normal DS18B20 interface with a few extra functions for testing
+**
+** @author Matias
+** @date   21/11/2024
+*/
+
+#ifndef __FAKE_DS18B20_H_
+#define __FAKE_DS18B20_H_
+
+#include "DS18B20.h"
+#include "fake_stm32xxxx_hal.h"
+
+#endif
