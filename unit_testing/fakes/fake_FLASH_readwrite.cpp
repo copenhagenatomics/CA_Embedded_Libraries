b18194b92d29cc2e585602575ecbb59721edbcd5
LukeWalker-CA <141138893+LukeWalker-CA@users.noreply.github.com>
2025-04-28 08:18:39 +0200

    Fake CRC and first flash read (#82)
    * Added CRC to fake flash to get correct behaviour if nothing is written in flash
    * Removed error on first read if nothing is written in flash for an address

--- unit_testing/fakes/fake_FLASH_readwrite.cpp
+++ unit_testing/fakes/fake_FLASH_readwrite.cpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <cstring>
 
+#include "crc.h"
 #include "fake_stm32xxxx_hal.h"
 #include "FLASH_readwrite.h"
 
@@ -19,21 +20,22 @@
 ***************************************************************************************************/
 
 #define SIZE_OF_MEM_ARRAY 256
+#define VALID_DATAPOINTS  10
 
 /***************************************************************************************************
 ** PRIVATE OBJECTS
 ***************************************************************************************************/
 
-static bool init_flag[VALID_SECTORS] = {true, true, true, true, true};
-static uint8_t mem_sectors[VALID_SECTORS][SIZE_OF_MEM_ARRAY] = {0}; 
-static uint32_t addresses[VALID_SECTORS] = {0}; 
+static int init_flag[VALID_DATAPOINTS] = {0};
+static uint8_t mem_sectors[VALID_DATAPOINTS][SIZE_OF_MEM_ARRAY] = {0}; 
+static uint32_t addresses[VALID_DATAPOINTS] = {0}; 
 static int num_addresses_used = 0;
 
 // Get the flash_address index to be used in mem_sectors
 // Assigns a "sector" in the mem_sectors array if not already initialised.
 int getFakeSector(uint32_t flash_address)
 {
-    for (int i = 0; i < VALID_SECTORS; i++)
+    for (int i = 0; i < VALID_DATAPOINTS; i++)
     {
         if (addresses[i] == flash_address)
         {
@@ -51,9 +53,9 @@ int getFakeSector(uint32_t flash_address)
 int writeToFlash(uint32_t flash_address, uint8_t *data, uint32_t size)
 {
     uint32_t sector = getFakeSector(flash_address);
-    if(init_flag[sector]) {
+    if(!init_flag[sector]) {
         std::fill_n(mem_sectors[sector], SIZE_OF_MEM_ARRAY, 0xFF);
-        init_flag[sector] = false;
+        init_flag[sector] = 1;
     }
 
     if (size < SIZE_OF_MEM_ARRAY) {
@@ -67,10 +69,9 @@ int writeToFlash(uint32_t flash_address, uint8_t *data, uint32_t size)
 int readFromFlash(uint32_t flash_address, uint8_t *data, uint32_t size)
 {
     uint32_t sector = getFakeSector(flash_address);
-    if(init_flag[sector]) {
+    if(!init_flag[sector]) {
         std::fill_n(mem_sectors[sector], SIZE_OF_MEM_ARRAY, 0xFF);
-        init_flag[sector] = false;
-        return 1;
+        init_flag[sector] = 1;
     }
 
     if (size < SIZE_OF_MEM_ARRAY) {
@@ -83,19 +84,37 @@ int readFromFlash(uint32_t flash_address, uint8_t *data, uint32_t size)
 // If this check is not included the compiler will throw an
 // error for projects where CRC module is not enabled
 #ifdef HAL_CRC_MODULE_ENABLED
+
 // Write data to FLASH_ADDR+indx including CRC.
 int writeToFlashCRC(CRC_HandleTypeDef *hcrc, uint32_t flash_address, uint8_t *data, uint32_t size)
 {
-    int ret = writeToFlash(flash_address, data, size);
+    uint8_t buf[SIZE_OF_MEM_ARRAY];
+    memcpy(buf, data, size);
+    initCrc8(0x00, 0x07);
+    uint8_t crc = crc8Calculate(buf, size);
+    buf[size] = crc;
+    size += 1; // Increase size to include CRC byte
+
+    /* Write the data with the CRC to flash */
+    int ret = writeToFlash(flash_address, buf, size);
     return ret;
 }
 
 
-// Read from flash without checking data integrity if  CRC module
-// is not enabled in project.
+/*!
+** @brief Read from flash and check CRC validity.
+*/
 int readFromFlashCRC(CRC_HandleTypeDef *hcrc, uint32_t flash_address, uint8_t *data, uint32_t size)
 {
-    int ret = readFromFlash(flash_address, data, size);
-    return ret;
+    uint8_t buf[SIZE_OF_MEM_ARRAY];
+
+    (void) readFromFlash(flash_address, buf, size + 1);
+
+    /* Real behaviour overwrites data even if CRC is not valid */
+    memcpy(data, buf, size);
+    initCrc8(0x00, 0x07);
+    uint8_t crc = crc8Calculate(buf, size);
+
+    return crc == buf[size] ? 0 : 1;
 }
 #endif
