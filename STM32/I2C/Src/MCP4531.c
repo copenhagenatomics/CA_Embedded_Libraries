2f6c39c154bbdc3473ae946a265304165f512bc0
TimotheDodinCA <timothe.dodin@copenhagenatomics.com>
2026-03-07 12:20:38 +0100

    Error code

--- STM32/I2C/Src/MCP4531.c
+++ STM32/I2C/Src/MCP4531.c
@@ -3,10 +3,10 @@
  * @brief   Driver file for MCP4x family of digital potentiometers/rheostats
  * @date    28/07/2025
  * @authors Luke Walker
- * 
+ *
  * Full set of Part numbers: MCP4531, MCP4532, MCP4541, MCP4542, MCP4551, MCP4552, MCP4561, MCP4562
  *                           MCP4631, MCP4632, MCP4641, MCP4642, MCP4651, MCP4652, MCP4661, MCP4662
- * 
+ *
  * Datasheet: https://ww1.microchip.com/downloads/en/DeviceDoc/22096b.pdf
  */
 
@@ -22,11 +22,11 @@
 
 #define MCP4X_WIPER_0_ADDR 0x00
 #define MCP4X_WIPER_1_ADDR 0x01
-#define MCP4X_STATUS_ADDR 0x05
+#define MCP4X_STATUS_ADDR  0x05
 
 #define MCP4X_WRITE_CMD 0x00
-#define MCP4X_READ_CMD 0x0C
-#define MCP4X_CMD_MSK 0x0C
+#define MCP4X_READ_CMD  0x0C
+#define MCP4X_CMD_MSK   0x0C
 
 /***************************************************************************************************
 ** PUBLIC FUNCTION DEFINITIONS
@@ -39,40 +39,50 @@
 ** @param hi2c   Handle for I2C peripheral
 ** @param i2c_address I2C address of the MCP4x device
 ** @param num_bits Number of bits for the digital potentiometer (7/8)
-** @param device_num Which device to initialise (valid for dual devices, always 0 for single devices)
+** @param device_num Which device to initialise (valid for dual devices, always 0 for single
+*devices)
 ** @return 0 on success, -1 on failure
 */
-int mcp4531_init(mcp4531_handle_t* handle, I2C_HandleTypeDef* hi2c, uint8_t i2c_address, uint8_t num_bits, uint8_t device_num) {
+int mcp4531_init(mcp4531_handle_t* handle, I2C_HandleTypeDef* hi2c, uint8_t i2c_address,
+                 uint8_t num_bits, uint8_t device_num) {
     if (!handle || !hi2c || num_bits > 8 || num_bits < 7 || device_num > 1) {
         return -1;
     }
 
-    handle->i2c_addr  = i2c_address;
-    handle->hi2c      = hi2c;
-    handle->num_bits  = num_bits;
-    handle->max_value = (1 << num_bits) + 1; /* Acc. datasheet, it is 129 and 257 bits */
+    handle->i2c_addr   = i2c_address;
+    handle->hi2c       = hi2c;
+    handle->num_bits   = num_bits;
+    handle->max_value  = (1 << num_bits) + 1; /* Acc. datasheet, it is 129 and 257 bits */
     handle->device_num = device_num;
+    handle->error      = false;
 
     /* Test read to verify  */
     uint8_t command = MCP4X_STATUS_ADDR << 4 | MCP4X_READ_CMD;
-    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(handle->hi2c, handle->i2c_addr << 1, &command, 1, 2);
+    HAL_StatusTypeDef status =
+        HAL_I2C_Master_Transmit(handle->hi2c, handle->i2c_addr << 1, &command, 1, 2);
     if (status != HAL_OK) {
-        return -2; // I2C communication error
+        handle->error = true;
+        return -2;  // I2C communication error
     }
 
     uint8_t data[2] = {0};
-    status = HAL_I2C_Master_Receive(handle->hi2c, handle->i2c_addr << 1, data, 2, 2);
+    status          = HAL_I2C_Master_Receive(handle->hi2c, handle->i2c_addr << 1, data, 2, 2);
 
     if (status != HAL_OK) {
-        return -3; // I2C communication error
+        handle->error = true;
+        return -3;  // I2C communication error
     }
 
-    /* Note: STATUS is fixed at 1F7h acc. datasheet for volatile devices. It is unclear from the 
-    ** datasheet if this is also the expected return value for non-volatile devices.In all 
+    /* Note: STATUS is fixed at 1F7h acc. datasheet for volatile devices. It is unclear from the
+    ** datasheet if this is also the expected return value for non-volatile devices.In all
     ** For MCP4531 prototypes the actual return value was 0x1F1
     ** For MCP4651 prototypes the actual return value was 0x1F0
     ** The reason for this is not known. */
-    return data[0] == 0x01 && (data[1] == 0xF0 || data[1] == 0xF1) ? 0 : -4;
+    int ret = data[0] == 0x01 && (data[1] == 0xF0 || data[1] == 0xF1) ? 0 : -4;
+    if (ret < 0) {
+        handle->error = true;
+    }
+    return ret;
 }
 
 /*!
@@ -89,15 +99,17 @@ int mcp4531_setWiperPos(mcp4531_handle_t* handle, uint16_t wiper_position) {
 
     uint8_t command = (handle->device_num == 0 ? MCP4X_WIPER_0_ADDR : MCP4X_WIPER_1_ADDR) << 4;
     command |= MCP4X_WRITE_CMD;
-    
+
     // MCP4651 expects 9-bit data: D8 in the LSB of the first byte
     uint8_t data[2];
     data[0] = command | ((wiper_position >> 8) & 0x01U);
     data[1] = wiper_position & 0xFFU;
 
-    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(handle->hi2c, handle->i2c_addr << 1, data, 2, 2);
+    HAL_StatusTypeDef status =
+        HAL_I2C_Master_Transmit(handle->hi2c, handle->i2c_addr << 1, data, 2, 2);
     
-    return (status == HAL_OK) ? 0 : -2;
+    handle->error = status != HAL_OK;
+    return handle->error ? 0 : -2;
 }
 
 /*!
@@ -114,22 +126,26 @@ int mcp4531_getWiperPos(mcp4531_handle_t* handle, uint16_t* wiper_position) {
 
     uint8_t command = (handle->device_num == 0 ? MCP4X_WIPER_0_ADDR : MCP4X_WIPER_1_ADDR) << 4;
     command |= MCP4X_READ_CMD;
-    HAL_StatusTypeDef status = HAL_I2C_Master_Transmit(handle->hi2c, handle->i2c_addr << 1, &command, 1, 2);
+    HAL_StatusTypeDef status =
+        HAL_I2C_Master_Transmit(handle->hi2c, handle->i2c_addr << 1, &command, 1, 2);
 
-    if(status != HAL_OK) {
+    if (status != HAL_OK) {
+        handle->error = true;
         return -2;
     }
 
     /* MCP4651 expects 9-bit data: D8 in the LSB of the first byte */
     uint8_t data[2] = {0};
-    status = HAL_I2C_Master_Receive(handle->hi2c, handle->i2c_addr << 1, data, 2, 2);
+    status          = HAL_I2C_Master_Receive(handle->hi2c, handle->i2c_addr << 1, data, 2, 2);
 
     if (status != HAL_OK) {
+        handle->error = true;
         return -3;
     }
 
     /* Combine the two bytes into a 9-bit value */
     *wiper_position = ((uint16_t)(data[0] & 0x01U) << 8) | ((uint16_t)data[1] & 0xFF);
 
+    handle->error = false;
     return 0;
 }
